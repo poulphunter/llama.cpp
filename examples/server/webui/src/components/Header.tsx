9a3e06f9d743d6a1549cadae52a1fa96a613ea44
poulphunter <>
2025-02-23 16:31:52 +0100

    Translation config in Header now

--- examples/server/webui/src/components/Header.tsx
+++ examples/server/webui/src/components/Header.tsx
@@ -6,7 +6,17 @@ import daisyuiThemes from 'daisyui/src/theming/themes';
 import { THEMES, CONFIG_DEFAULT, isDev } from '../Config';
 import { useNavigate } from 'react-router';
 import { useTranslation } from 'react-i18next';
-import LanguageSelector from './LanguageSelector.tsx';
+import i18next from 'i18next';
+
+type languageOption = { language: string; code: string };
+
+const languageOptions: languageOption[] = [
+  { language: 'English', code: 'en' },
+  { language: 'French', code: 'fr' },
+  { language: 'German', code: 'de' },
+  { language: 'Spanish', code: 'es' },
+  { language: 'Italian', code: 'it' },
+];
 
 const PROMPT_JSON = [
   {
@@ -17,11 +27,18 @@ const PROMPT_JSON = [
 ];
 
 export default function Header() {
-  const { t } = useTranslation();
+  const [lang, setLanguage] = useState(i18next.language);
+  const { t, i18n } = useTranslation();
+  if (isDev) {
+    console.log(lang);
+  }
+  useEffect(() => {
+    document.body.dir = i18n.dir();
+  }, [i18n, i18n.language]);
+
   const navigate = useNavigate();
   const [selectedTheme, setSelectedTheme] = useState(StorageUtils.getTheme());
   const { setShowSettings, closeDropDownMenu } = useAppContext();
-  const [selectedConfig, setSelectedConfig] = useState<number>(-1);
 
   const setTheme = (theme: string) => {
     StorageUtils.setTheme(theme);
@@ -39,6 +56,7 @@ export default function Header() {
   const [promptSelectConfig, setPromptSelectConfig] = useState<
     typeof PROMPT_JSON | null
   >(null);
+  const [selectedConfig, setSelectedConfig] = useState<number>(-1);
   useEffect(() => {
     document.body.setAttribute('data-theme', selectedTheme);
     document.body.setAttribute(
@@ -56,8 +74,11 @@ export default function Header() {
         if (data && data.prompts) {
           setPromptSelectConfig(data.prompts);
           Object.keys(data.prompts).forEach(function (key) {
-            const name = data.prompts[key].name;
-            prt.push({ key: parseInt(key), value: name });
+            console.log(lang);
+            if (lang == data.prompts[key].lang) {
+              const name = data.prompts[key].name;
+              prt.push({ key: parseInt(key), value: name });
+            }
           });
         }
         setPromptSelectOptions(prt);
@@ -67,7 +88,7 @@ export default function Header() {
           console.log(error);
         }
       });
-  }, []);
+  }, [lang]);
   useEffect(() => {
     if (promptSelectConfig !== null && selectedConfig == -1) {
       setSelectedConfig(0);
@@ -84,7 +105,7 @@ export default function Header() {
   const removeConversation = () => {
     if (isCurrConvGenerating || !viewingChat) return;
     const convId = viewingChat?.conv.id;
-    if (window.confirm(t('conversations.deleteConfirm'))) {
+    if (window.confirm(t('Header.deleteConfirm'))) {
       StorageUtils.remove(convId);
       navigate('/');
     }
@@ -130,14 +151,14 @@ export default function Header() {
         );
         if (mustBeString) {
           if (!isString(val)) {
-            alert(`Value for ${key} must be string`);
+            console.log(`Value for ${key} must be string`);
             return;
           }
         } else if (mustBeNumeric) {
           const trimedValue = val.toString().trim();
           const numVal = Number(trimedValue);
           if (isNaN(numVal) || !isNumeric(numVal) || trimedValue.length === 0) {
-            alert(`Value for ${key} must be numeric`);
+            console.log(`Value for ${key} must be numeric`);
             return;
           }
           // force conversion to number
@@ -145,7 +166,7 @@ export default function Header() {
           newConfig[key] = numVal;
         } else if (mustBeBoolean) {
           if (!isBoolean(val)) {
-            alert(`Value for ${key} must be boolean`);
+            console.log(`Value for ${key} must be boolean`);
             return;
           }
         } else {
@@ -209,16 +230,19 @@ export default function Header() {
               className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
             >
               <li onClick={downloadConversation}>
-                <a>Download</a>
+                <a>{t('Header.downloadBtn')}</a>
               </li>
               <li className="text-error" onClick={removeConversation}>
-                <a>Delete</a>
+                <a>{t('Header.deleteBtn')}</a>
               </li>
             </ul>
           </div>
         )}
         {promptSelectOptions.length > 0 ? (
-          <div className="tooltip tooltip-bottom" data-tip="Settings">
+          <div
+            className="tooltip tooltip-bottom"
+            data-tip={t('Header.tooltipSettings')}
+          >
             <div className="dropdown dropdown-end dropdown-bottom">
               <div tabIndex={0} role="button" className="btn m-1">
                 {/* settings button */}
@@ -244,7 +268,7 @@ export default function Header() {
                     name="settings"
                     className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
                     aria-label="Manual settings"
-                    value="Manual settings"
+                    value={t('Header.manualSettings')}
                     onChange={() => setShowSettings(true)}
                   />
                 </li>
@@ -289,7 +313,10 @@ export default function Header() {
         )}
 
         {/* theme controller is copied from https://daisyui.com/components/theme-controller/ */}
-        <div className="tooltip tooltip-bottom" data-tip="Themes">
+        <div
+          className="tooltip tooltip-bottom"
+          data-tip={t('Header.tooltipTheme')}
+        >
           <div className="dropdown dropdown-end dropdown-bottom">
             <div tabIndex={0} role="button" className="btn m-1">
               <svg
@@ -334,7 +361,50 @@ export default function Header() {
             </ul>
           </div>
         </div>
-        <LanguageSelector></LanguageSelector>
+        <div
+          className="tooltip tooltip-bottom"
+          data-tip={t('Header.tooltipLanguage')}
+        >
+          <div className="dropdown dropdown-end dropdown-bottom">
+            <div tabIndex={0} role="button" className="btn m-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M11 5a.75.75 0 0 1 .688.452l3.25 7.5a.75.75 0 1 1-1.376.596L12.89 12H9.109l-.67 1.548a.75.75 0 1 1-1.377-.596l3.25-7.5A.75.75 0 0 1 11 5Zm-1.24 5.5h2.48L11 7.636 9.76 10.5ZM5 1a.75.75 0 0 1 .75.75v1.261a25.27 25.27 0 0 1 2.598.211.75.75 0 1 1-.2 1.487c-.22-.03-.44-.056-.662-.08A12.939 12.939 0 0 1 5.92 8.058c.237.304.488.595.752.873a.75.75 0 0 1-1.086 1.035A13.075 13.075 0 0 1 5 9.307a13.068 13.068 0 0 1-2.841 2.546.75.75 0 0 1-.827-1.252A11.566 11.566 0 0 0 4.08 8.057a12.991 12.991 0 0 1-.554-.938.75.75 0 1 1 1.323-.707c.049.09.099.181.15.271.388-.68.708-1.405.952-2.164a23.941 23.941 0 0 0-4.1.19.75.75 0 0 1-.2-1.487c.853-.114 1.72-.185 2.598-.211V1.75A.75.75 0 0 1 5 1Z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl h-80 overflow-y-auto"
+            >
+              {languageOptions.map(({ language, code }, key) => (
+                <li key={key}>
+                  <input
+                    type="radio"
+                    name="theme-dropdown"
+                    className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                    aria-label={language}
+                    value={language}
+                    onChange={() => {
+                      setLanguage(code);
+                      i18next.changeLanguage(code);
+                    }}
+                    onClick={() => {
+                      closeDropDownMenu('');
+                    }}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
